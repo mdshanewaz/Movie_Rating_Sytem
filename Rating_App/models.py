8f02dedd8812efaa36fda5b0bde2468bf5d1d44b
MD. SHA NEWAZ <70477038+mdshanewaz@users.noreply.github.com>
2024-04-03 21:58:28 +0600

    Update models.py

--- Rating_App/models.py
+++ Rating_App/models.py
@@ -10,9 +10,9 @@ class MovieUserRatingModel(models.Model):
     (0.0, 0.0), (0.1, 0.1), (0.2, 0.2), (0.3, 0.3), (0.4, 0.4), (0.5, 0.5), (0.6, 0.6), (0.7, 0.7), (0.8, 0.8), (0.9, 0.9), (1.0, 1.0), (1.1, 1.1), (1.2, 1.2), (1.3, 1.3), (1.4, 1.4), (1.5, 1.5), (1.6, 1.6), (1.7, 1.7), (1.8, 1.8), (1.9, 1.9), (2.0, 2.0), (2.1, 2.1), (2.2, 2.2), (2.3, 2.3), (2.4, 2.4), (2.5, 2.5), (2.6, 2.6), (2.7, 2.7), (2.8, 2.8), (2.9, 2.9), (3.0, 3.0), (3.1, 3.1), (3.2, 3.2), (3.3, 3.3), (3.4, 3.4), (3.5, 3.5), (3.6, 3.6), (3.7, 3.7), (3.8, 3.8), (3.9, 3.9), (4.0, 4.0), (4.1, 4.1), (4.2, 4.2), (4.3, 4.3), (4.4, 4.4), (4.5, 4.5), (4.6, 4.6), (4.7, 4.7), (4.8, 4.8), (4.9, 4.9), (5.0, 5.0),
 )
 
-    user = models.ForeignKey(CustomUesr, on_delete=models.CASCADE, null=True)
-    movie = models.ForeignKey(MovieModel, on_delete=models.CASCADE, null=True)
-    rate = models.FloatField(choices=RATE_CHOICES, null=True)
+    user_id = models.ForeignKey(CustomUesr, on_delete=models.CASCADE, null=True)
+    movie_id = models.ForeignKey(MovieModel, on_delete=models.CASCADE, null=True)
+    rating = models.FloatField(choices=RATE_CHOICES, null=True)
     
     def __str__(self):
         return str(self.rate)
